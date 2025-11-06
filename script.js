a734f76b9a60e83a3795cdaa69a48145faa47e16
salemtv <yeahneo21@gmail.com>
2025-11-05 22:33:47 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -471,7 +471,7 @@ function renderEnVi() {
           <div class="selector-left">
             <span style="font-size:22px" class="material-symbols-outlined">home_max</span>
             <span class="selected-text highlight">L1 Max</span>
-            <span id="liveBadge" class="live-badge"><span class="dot">●</span> LIVE</span>
+            <span id="liveBadge" class="live-badge"><span class="dot">•</span> LIVE</span>
           </div>
           <span class="material-symbols-outlined arrow-toggle">expand_more</span>
         </div>
